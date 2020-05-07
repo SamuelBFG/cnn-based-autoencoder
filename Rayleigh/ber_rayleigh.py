1aa2d7d76817c4a845fba5412b81cbf1017fc664
SamuelBFG <samuelbfgomes@gmail.com>
2020-05-07 16:49:36 -0300

    rayleigh ber

--- Rayleigh/ber_rayleigh.py
+++ Rayleigh/ber_rayleigh.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
   
 N = 100000
 EbNodB_range = range(0, 36)
-ber = [None]*len(EbNodB_range)
+itr = len(EbNodB_range)
+ber = [None]*itr
 tx_symbol = 0
 noise = 0 
 ch_coeff = 0
 rx_symbol = 0 
 det_symbol = 0 
 
-for n in range (0, len(EbNodB_range)): 
+for n in range (0, itr): 
  
     EbNodB = EbNodB_range[n]   
     EbNo=10.0**(EbNodB/10.0)
@@ -19,13 +20,13 @@ for n in range (0, len(EbNodB_range)):
     noise_mean = 0
     
     no_errors = 0
-
-    tx_symbol = 2*random.randint(0,N)-1
-    noise = random.gauss(noise_mean, noise_std)
-    ch_coeff = sqrt(random.gauss(0,1)**2+random.gauss(0,1)**2)/sqrt(2)
-    rx_symbol = tx_symbol*ch_coeff + noise
-    det_symbol = 2 * (rx_symbol >= 0) - 1
-    no_errors += 1*(tx_symbol != det_symbol)  
+    for m in range (0, N):
+      tx_symbol = 2*random.randint(0,1)-1
+      noise = random.gauss(noise_mean, noise_std)
+      ch_coeff = sqrt(random.gauss(0,1)**2+random.gauss(0,1)**2)/sqrt(2)
+      rx_symbol = tx_symbol*ch_coeff + noise
+      det_symbol = 2 * (rx_symbol >= 0) - 1
+      no_errors += 1*(tx_symbol != det_symbol)  
     
     ber[n] = no_errors / N
     print ("EbNodB:", EbNodB)
@@ -33,7 +34,7 @@ for n in range (0, len(EbNodB_range)):
     print ("Error probability:", ber[n] )
         
 plt.plot(EbNodB_range, ber, 'bo-')
-plt.axis([0, 35, 1e-5, 0.1])
+plt.axis([0, 35, 1e-06, 0.1])
 plt.xscale('linear')
 plt.yscale('log')
 plt.xlabel('EbNo(dB)')
